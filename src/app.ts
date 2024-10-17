185da8510780c37399b60e084eefe28b03633800
yourunb <bxyura@gmail.com>
2024-10-17 10:35:35 +0300

    feat: add work express fir routes page

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import { userRouter } from './routes';
 
 dotenv.config();
 
-export const startServer = (port: number) => { 
+export const startServer = (port: number) => {
+  const app = express();
+
+  app.use(express.json());
+  app.use('/routes', userRouter);
+
   express().use(express.json());
   express().use((req, res) => {
     res.status(404).json({ message: 'Not Found' });
   });
-  express().listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+  express().listen(port, () => console.log(`Server work: http://localhost:${port}`));
 }
