864e4e593118355fb7f4fcb27fafda20fdf0b222
yourunb <bxyura@gmail.com>
2024-10-17 11:15:53 +0300

    feat: add route for put user

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -11,6 +11,8 @@ interface User {
 
 let users: User[] = [];
 
+const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
 router.get('/', (req: Request, res: Response) => {
   res.status(200).json(users);
 });
@@ -18,12 +20,31 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/:userId', (req: any, res: any) => {
   const { userId } = req.params;
   const foundUser = users.find(user => user.id === userId);
-  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
   if (uuidRegex) return res.status(400).json({ message: 'not uuid' });
   if (!foundUser) return res.status(404).json({ message: `doesn't exist` });
 
   res.status(200).json(foundUser);
+});
+
+router.put('/:userId', (req: any,res: any) => {
+  const {
+    userId
+  } = req.params;
+  const i = users.findIndex(user => user.id === userId);
+
+  if (!uuidRegex.test(userId)) return res.status(400).json({ message: 'not uuid' });
+  if (i === -1) return res.status(404).json({ message: `doesn't exist` });
+
+  const { username, age, hobbies } = req.body;
+  users[i] = {
+    ...users[i],
+    ...(username !== undefined && { username }),
+    ...(age !== undefined && { age }),
+    ...(hobbies !== undefined && { hobbies }),
+  };
+
+  res.status(200).json(users[i]);
 });
 
 export const userRouter = router;
